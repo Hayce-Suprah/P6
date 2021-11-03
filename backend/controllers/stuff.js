9591e40f771348210fcffa9e14bf725249d93428
Nicolas Maffar <nicolas.maffar@gmail.com>
2021-11-03 22:33:03 +0100

    roots

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -1,12 +1,13 @@
 const Sauces = require('../models/Sauces');
 const fs = require('fs');
+const { findOne } = require('../models/Sauces');
 
 
 exports.getAllStuff = (req, res, next) => {
 
-    Thing.find().then(
-      (things) => {
-        res.status(200).json(things);
+    Sauces.find().then(
+      (sauces) => {
+        res.status(200).json(sauces);
       }
     ).catch(
       (error) => {
@@ -18,8 +19,11 @@ exports.getAllStuff = (req, res, next) => {
   };
 
   exports.createThing = (req, res, next) => {
-      const sauceObject = req.body.sauce;
-      const sauce = new Sauces(sauceObject);
+      const sauceObject = JSON.parse(req.body.sauce);
+      const sauce = new Sauces({
+        ...sauceObject,
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      });  
       sauce.save()
         .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
         .catch(error => res.status(400).json({ error }));
@@ -37,11 +41,11 @@ exports.getAllStuff = (req, res, next) => {
   };
   
   exports.getOneThing = (req, res, next) => {
-    Thing.findOne({
+    Sauces.findOne({
       _id: req.params.id
     }).then(
-      (thing) => {
-        res.status(200).json(thing);
+      (sauces) => {
+        res.status(200).json(sauces);
       }
     ).catch(
       (error) => {
@@ -53,22 +57,22 @@ exports.getAllStuff = (req, res, next) => {
   };
   
   exports.modifyThing = (req, res, next) => {
-    const thingObject = req.file ?
+    const sauceObject = req.file ?
       {
-        ...JSON.parse(req.body.thing),
+        ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
       } : { ...req.body };
-    Thing.updateOne({ _id: req.params.id }, { ...thingObject, _id: req.params.id })
+    Sauces.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Objet modifié !'}))
       .catch(error => res.status(400).json({ error }));
   };
   
   exports.deleteThing = (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-      .then(thing => {
-        const filename = thing.imageUrl.split('/images/')[1];
+    Sauces.findOne({ _id: req.params.id })
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
         fs.unlink(`images/${filename}`, () => {
-          Thing.deleteOne({ _id: req.params.id })
+          Sauces.deleteOne({ _id: req.params.id })
             .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
             .catch(error => res.status(400).json({ error }));
         });
@@ -76,3 +80,81 @@ exports.getAllStuff = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   };
   
+  exports.modifyLike = (req, res, next) => {
+    let message;
+    Sauces.findOne({
+      _id: req.params.id
+    }).then(
+      (sauce) => {
+        console.log(req.body.like)
+        if(req.body.like === '1'){
+          const finded = sauce.usersLiked.find(id => id === req.body.userId)
+          if(!finded){
+            sauce.usersLiked.push(req.body.userId)
+            sauce.likes = sauce.likes++
+            message = "L'utilisateur à liker la sauce"
+          }else{
+            message = "L'utilisateur à déjà liker la sauce"
+          }
+          
+        }else if(req.body.like === '0'){
+          const finded = sauce.usersLiked.find(id => id === req.body.userId)
+          if(!finded){
+            sauce.usersLiked.remove(req.body.userId)
+            sauce.likes = sauce.likes -1
+            message = "L'utilisateur à annuler son like"
+          }else{
+            sauce.usersDisliked.remove(req.body.userId)
+            sauce.dislikes = sauce.dislikes -1
+            message =  "L'utilisateur à annuler son dislike"
+          }
+          
+        }else if(req.body.like === '-1'){
+          const finded = sauce.usersDisliked.find(id => id === req.body.userId)
+          if(!finded){
+            sauce.usersDisliked.push(req.body.userId)
+            sauce.dislikes = sauce.dislikes +1
+            message = "L'utilisateur à disliker la sauce"
+          }else{
+            message = "L'utilisateur à déjà disliker la sauce"
+          }
+
+        }
+        sauce
+        .save()
+    
+        .then(() => res.status(200).json({ message: message}))
+      }
+    )
+
+    .catch(
+      (error) => {
+        res.status(404).json({
+          error: error
+        });
+      }
+    );
+
+    //Récupérer le détails de la sauce avec son ID
+
+    //Regarder l'état de l'attribut like qui est passé dans le body
+    /*
+    * Si like = 1 --> L'utilisateur veut liker
+      Si like = 0 --> L'utilisateur veut enlever son like
+      Si like = -1 --> L'utilisateur veut dislike
+    */
+
+    //Si l'utilisateur veut liker donc (like = 1)
+    //Parcourir le tableau UserLike et ajouter l'id userId SI il n'existe pas 
+    //Si le userId n'existe pas, on ajouter le userId dans le tableau UserLike ET incrémenter la valeur likes
+
+    //Si l'utilisateur veut annuler son like (like = 0)
+    //Parcour le tableau UserLike et si l'id est présent, on enleve l'id de l'utilisateur du tableau UserLike
+    //Si l'id est présent, on décrémente à la valeur likes
+
+    //Si l'utilisateur veut dislike donc (like = -1)
+    //Parcourir le tableau UserDislike et ajouter l'id userId SI il n'existe pas 
+    //Si le userId n'existe pas, on ajouter le userId dans le tableau UserDislke ET incrémenter la valeur dislikes
+
+
+  }
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+
 const stuffCtrl = require('../controllers/stuff');
 
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
 
 router.get('/sauces', auth, stuffCtrl.getAllStuff);
-router.post('/sauces', auth, stuffCtrl.createThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.put('/:id', auth, stuffCtrl.modifyThing);
-router.delete('/:id', auth, stuffCtrl.deleteThing);
+router.post('/sauces', auth, multer, stuffCtrl.createThing);
+router.get('/sauces/:id', auth, stuffCtrl.getOneThing);
+router.put('/sauces/:id', auth, multer, stuffCtrl.modifyThing);
+router.delete('/sauces/:id', auth, stuffCtrl.deleteThing);
+router.post('/sauces/:id/like', multer, stuffCtrl.modifyLike);
 
 
 
